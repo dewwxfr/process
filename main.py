f82e0b4b79d65a83e0e9d57ed7fae590d7e1b589
lua3x <106031028+dewwxfr@users.noreply.github.com>
2023-09-07 13:20:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import dearpygui.dearpygui as gui
+import socket
 import pymem
+
+hostname = socket.gethostname()
 
 print('-------------------')
 
 pm = pymem.Pymem('RobloxPlayerBeta.exe')
-print(f'process base: {pm.process_base}')
+print(f'\nprocess base: {pm.process_base}')
 print(f'process handle: {pm.process_handle}')
 print(f'process id: {pm.process_id}')
+print(f'your pc name : {hostname}')
 
-print('-------------------')
+print('\n-------------------')
 
 gui.create_context()
 gui.create_viewport(title='[+]', width=300, height=360)
@@ -21,10 +25,11 @@ with gui.window(label='[+]', width=1000, height=360, no_title_bar=True, no_resiz
             gui.add_button(label=f'process id: {pm.process_id}', tag='blehh')
             gui.add_button(label=f'process handle: {pm.process_handle}', tag='blehh1')
             gui.add_button(label=f'process base: {pm.process_base}', tag='blehh2')
+            gui.add_button(label=f'your pc name : {hostname}', tag='blehh3')
 
 gui.show_viewport()
 gui.start_dearpygui()
 gui.destroy_context()
 
-input("\npress any key to close...")
+input("\npress enter to close...")
 [input(i) for i in range(-2, 0, -3)]
